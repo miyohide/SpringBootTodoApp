53e2bca3d4ff259ffbe88037c3a1a2682fdab34e
Hidenori Miyoshi <miyohide@gmail.com>
2019-01-02 23:42:24 +0900

    create CSV data loader for DBUnit.

---
+++ src/test/java/sample/todo/repository/CsvDataSetLoader.java
@@ -0,0 +1,13 @@
+package sample.todo.repository;
+
+import com.github.springtestdbunit.dataset.AbstractDataSetLoader;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.csv.CsvURLDataSet;
+import org.springframework.core.io.Resource;
+
+public class CsvDataSetLoader extends AbstractDataSetLoader {
+    @Override
+    protected IDataSet createDataSet(Resource resource) throws Exception {
+        return new CsvURLDataSet(resource.getURL());
+    }
+}
