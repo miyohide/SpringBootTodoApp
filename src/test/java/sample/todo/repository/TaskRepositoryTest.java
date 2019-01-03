239c58e8123e5275be46aaf9e235c570df8d48e3
Hidenori Miyoshi <miyohide@gmail.com>
2019-01-03 14:56:02 +0900

    add repository test using dbsetup.

---
+++ src/test/java/sample/todo/repository/TaskRepositoryTest.java
@@ -0,0 +1,61 @@
+package sample.todo.repository;
+
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.destination.DataSourceDestination;
+import com.ninja_squad.dbsetup.operation.Operation;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import sample.todo.domain.Task;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static com.ninja_squad.dbsetup.Operations.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@JdbcTest
+@ComponentScan
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+})
+public class TaskRepositoryTest {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void findAllTest() {
+        final Operation DELETE_ALL = deleteAllFrom("tasks");
+        final Operation INSERT =
+                insertInto("tasks")
+                .row()
+                .column("id", 2)
+                .column("subject", "subject2")
+                .column("deadLine", "2018-12-4")
+                .column("hasDone", false)
+                .end()
+                .row()
+                .column("id", 1)
+                .column("subject", "subject1")
+                .column("deadLine", "2018-12-3")
+                .column("hasDone", false)
+                .end().build();
+        Operation operation = sequenceOf(DELETE_ALL, INSERT);
+        DbSetup dbSetup = new DbSetup(new DataSourceDestination(dataSource), operation);
+        dbSetup.launch();
+        List<Task> actual = taskRepository.findAll();
+        assertThat(actual.size(), is(2));
+        assertThat(actual.get(0).getId(), is(1));
+        assertThat(actual.get(1).getId(), is(2));
+    }
+}
